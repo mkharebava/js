b9f58fee7201a34630364e08cb16d16341e2365d
mkharebava <m_kharebava4@cu.edu.ge>
2024-10-30 00:11:47 +0400

    js

---
+++ script.js
@@ -0,0 +1,11 @@
+let Name = "mariam";
+let SurName= "kh";
+let Age = 20;
+let Gmail="burd@gmail.com"
+let PhoneNmb=555555555;
+let IsAdult=false;
+
+console.log("name is", Name, "surname is", SurName, "age is", Age, "gmail is", Gmail, "numbers is");
+console.log(" phone number is %c%s", "color:red", PhoneNmb);
+console.log(" isadult %c%s", "color:yellow", IsAdult);
+console.log(typeof IsAdult);
